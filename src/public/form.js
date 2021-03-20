5e4f544a5558e96dbc527f9f010686366f1ba065
slbejaranom <slbejaranom@gmail.com>
2021-03-19 23:04:27 -0500

    :zap:Cambios a la interfaz

--- src/public/form.js
+++ src/public/form.js
@@ -1,19 +1,18 @@
 // This is our index.js file
-
-/*
+$(document).ready(() => {
+  /*
 Create an instance of the secure fields. Pass the public key as the first argument. 
 As the second argument, we'll pass a list of custom fonts to be applied to the fields (this is optional).
 */
-const fonts = [
+  const fonts = [
     {
-      src: 'https://fonts.googleapis.com/css?family=Source+Code+Pro',
-    }
-  ]
+      src: "https://fonts.googleapis.com/css?family=Source+Code+Pro",
+    },
+  ];
   const formElements = new POS.Fields("d2a0e6e9-526b-434f-840b-90de8a550205", {
-    fonts
-  })
-  
-  
+    fonts,
+  });
+
   /*
   Create an object holding additional options that you can pass to the constructor for instantiating 
   the credit card and card expiry fields.
@@ -21,44 +20,50 @@ const fonts = [
   but to keep it simple we'll just show this one object in our example. 
   */
   const placeholders = {
-      cardNumber: '1234 1234 1234 1234',
-      expDate: 'MM / YY'
-  }
-  
+    cardNumber: "1234 1234 1234 1234",
+    expDate: "MM / YY",
+  };
+
   // Instantiate the fields you want to show and mount them to the DOM.
-  const cardNumber = formElements.create('cardNumber', {
-      placeholders
-  })
-  cardNumber.mount('#card-number')
-  
-  const expiry = formElements.create('creditCardExpiry', {
-      placeholders
-  })
-  expiry.mount('#exp-date')
-  
+  const cardNumber = formElements.create("cardNumber", {
+    placeholders,
+  });
+  cardNumber.mount("#card-number");
+
+  const expiry = formElements.create("creditCardExpiry", {
+    placeholders,
+  });
+  expiry.mount("#exp-date");
+
   /*
   Create a token when the user submits the form, but not until we fetched the card holder's name 
   so that we can pass it in an additional data object to the createToken call.
   */
-  document.getElementById('payment-form').addEventListener('submit', async(event) => {
-      event.preventDefault()
+  document
+    .getElementById("payment-form")
+    .addEventListener("submit", async (event) => {
+      Swal.fire({
+        icon: "success",
+        text: "Presione click para continuar",
+        title: "Pago aceptado"
+      });
+      event.preventDefault();
       const additionalData = {
-          holder_name: document.getElementById('cardholder-name').value // This field is mandatory
-      }
+        holder_name: document.getElementById("cardholder-name").value, // This field is mandatory
+      };
       const result = await POS.createToken(cardNumber, {
-          additionalData,
-          environment: 'test' // Set the PaymentsOS environment you're connecting to
-      })
+        additionalData,
+        environment: "test" // Set the PaymentsOS environment you're connecting to
+      });
       console.log(`The response is ${JSON.stringify(result)}`);
-      let respuesta = JSON.parse(result)
+      let respuesta = JSON.parse(result);
       console.log(respuesta.token);
 
-      
-
       $.ajax({
-        url: 'http://localhost:5000',
-        type: 'POST',
-        contentType: 'application/json',
+        url: "http://localhost:5000",
+        type: "POST",
+        contentType: "application/json",
         data: result
       });
-  })
+    });
+});
